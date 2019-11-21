de76a0ca8265441f27e7feb00b2158eb0251c0e2
JustChewie <30376684+JustChewie@users.noreply.github.com>
2019-11-21 12:51:26 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 string coord[3][3] = { {"0,0","0,1","0,2"},{"1,0","1,1","1,2"},{"2,0","2,1","2,2"} }; // Main 2d array for the game, i'll regret declaring it as a global var later
 
 void init(), reset(); // functions' prototypes
+void cinFail(int &fcoord, string fplayer, int fcounter);
 int checkWin();
 string confirm; // string to confirm if the player wants a rematch (see ln 129)
 
@@ -17,30 +18,31 @@ rematch: // Rematch goto (see ln 134)
 
 	while (over != 9) { // Main while loop which contains two smaller while loops for player rotation
 		while (true) {
-
+			
+			int coordCounter = 1;
+			string playername = "Player 1 (X)";
 			init();
-
-			cout << "Player 1 (X): " << endl;
-		cin_coord1_X:
-
-			cout << "Coord 1: ";
+			
+			cout << playername << ":" << endl;
+			cout << "Coord " << coordCounter << ": ";
 			cin >> coord1;
-			if (coord1 > 2 || coord1 < 0) {
-				cout << "Invalid input" << endl;
-				system("pause");
-				init();
-				goto cin_coord1_X;
-			}
-		cin_coord2_X:
 
-			cout << "Coord 2: ";
+			if (coord1 > 2 || coord1 < 0) {
+				cinFail(coord1, playername, coordCounter);
+			}
+
+			else {
+				init();
+			}
+
+			coordCounter++;
+
+			cout << playername << ":" << endl;
+			cout << "Coord " << coordCounter << ": ";
 			cin >> coord2;
 
 			if (coord2 > 2 || coord2 < 0) {
-				cout << "Invalid input" << endl;
-				system("pause");
-				init();
-				goto cin_coord2_X;
+				cinFail(coord2, playername, coordCounter);
 			}
 
 			if (coord[coord1][coord2] == " X " || coord[coord1][coord2] == " O ") {
@@ -59,11 +61,11 @@ rematch: // Rematch goto (see ln 134)
 					break;
 			}
 
-			init();
+			//init();
 
 		}
 
-		init();
+		//init();
 
 		if (over == 9) {
 			cout << "You are out of turns. Draw." << endl;
@@ -71,30 +73,29 @@ rematch: // Rematch goto (see ln 134)
 		}
 
 		while (true) {
+			int coordCounter = 1;
+			string playername = "Player 2 (O)";
 			init();
 
-			cout << "Player 2 (O): " << endl;
-
-		cin_coord1_O:
-
-			cout << "Coord 1: ";
+			cout << playername << ":" << endl;
+			cout << "Coord " << coordCounter << ": ";
 			cin >> coord1;
-			if (coord1 > 2 || coord1 < 0) {
-				cout << "Invalid input" << endl;
-				system("pause");
-				init();
-				goto cin_coord1_O;
-			}
-		cin_coord2_O:
 
-			cout << "Coord 2: ";
+			if (coord1 > 2 || coord1 < 0) {
+				cinFail(coord1, playername, coordCounter);
+			}
+
+			else {
+				init();
+			}
+			coordCounter++;
+
+			cout << playername << ":" << endl;
+			cout << "Coord " << coordCounter << ": ";
 			cin >> coord2;
 
 			if (coord2 > 2 || coord2 < 0) {
-				cout << "Invalid input" << endl;
-				system("pause");
-				init();
-				goto cin_coord2_O;
+				cinFail(coord1, playername, coordCounter);
 			}
 
 			if (coord[coord1][coord2] == " X " || coord[coord1][coord2] == " O ") {
@@ -107,31 +108,31 @@ rematch: // Rematch goto (see ln 134)
 				over++;
 				init();
 				if (checkWin() == 1) {
-					goto propose;
+					goto propose; // see ln 127
 				}
 
 				else
 					break;
 			}
 
-			init();
+			//init();
 		}
 
-		init();
+		//init();
 
-		if (over == 9) {
+		if (over == 9) { // Reserve if statement for 100% end of the loop
 			cout << "You are out of turns. Draw." << endl;
-			break;
+			break; // breaking the loop
 		}
 	}
-propose:
+propose: // Label propose (see ln 56, 110)
 
-	cout << "Want to rematch? (y/n): ";
+	cout << "Want to rematch? (y/n): "; // Asking the player if he wants to rematch
 	cin >> confirm;
 
-	if (confirm == "y" || confirm == "Y") {
+	if (confirm == "y" || confirm == "Y") { // If the player types 'y' or 'Y' then executing goto
 		
-		goto rematch;
+		goto rematch; // Going to rematch (see ln 13)
 	}
 
 	return 0;
@@ -139,10 +140,10 @@ propose:
 
 void init() { // Main menu (board) for the game
 	system("cls"); // Clears the console
-	cout << "tictactoe Alpha 0.1\n\n";
-	cout << "By Artem B.\n\n\n";
+	cout << "tictactoe Alpha 0.1\n\n"; // Name of the project
+	cout << "By Artem B.\n\n\n"; // My info
 
-	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl; // The board itself
 	cout << endl;
 	cout << "     |     |     " << endl;
 	cout << " " << coord[0][0] << " | " << coord[0][1] << " | " << coord[0][2] << endl;
@@ -217,4 +218,21 @@ void reset() { // Reset function for the main array because it is a global varia
 	coord[2][0] = "2,0";
 	coord[2][1] = "2,1";
 	coord[2][2] = "2,2";
+}
+
+void cinFail(int &fcoord, string fplayer, int fcounter) {
+	cout << "Invalid input" << endl;
+	system("pause");
+	init();
+	while (fcoord > 2 || fcoord < 0) {
+		cout << fplayer << ":" << endl;
+		cout << "Coord " << fcounter << ": ";
+		cin >> fcoord;
+		if (fcoord > 2 || fcoord < 0) {
+			cout << "Invalid input" << endl;
+			system("pause");
+		}
+		init();
+	}
+	
 }
